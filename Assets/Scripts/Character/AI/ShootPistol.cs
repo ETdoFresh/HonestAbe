3b9d9b69f8fa6341fe15b651aeda8e0831b1deb8
Rachel Farrell <rfarrell@uno.edu>
2016-04-20 15:24:41 -0500

    Add some pistol AI stuff

---
+++ Assets/Scripts/Character/AI/ShootPistol.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using BehaviourMachine;
+
+public class ShootPistol : ConditionNode
+{
+	private PistolAttack pistolAttack;
+
+	public override void OnEnable()
+	{
+		pistolAttack = self.GetComponent<PistolAttack>();
+		pistolAttack.StartLightAttack ();
+	}
+
+	public override Status Update()
+	{
+		if (pistolAttack.state == BaseAttack.State.Null)
+		{
+			if (onSuccess.id != 0)
+				owner.root.SendEvent(onSuccess.id);
+			return Status.Success;
+		}
+
+		return Status.Running;
+	}
+}
